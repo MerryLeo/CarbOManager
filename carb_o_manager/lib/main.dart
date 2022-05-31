ffafa0d12309226b3cf00409aa91b3ea29ed3f52
Léo Taillon <leotaillon2@gmail.com>
2022-05-31 14:18:09 -0400

    Added Basic UI and Class

--- carb_o_manager/lib/main.dart
+++ carb_o_manager/lib/main.dart
@@ -3,6 +3,9 @@
 */
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'meal_page.dart';
+import 'ingredient_page.dart';
+import 'health_page.dart';
 import 'main_view.dart';
 
 void main() {
@@ -10,9 +13,32 @@ void main() {
       create: (_) => MyAppModel(), child: const MyApp()));
 }
 
-class MyAppModel with ChangeNotifier {}
+class MyAppModel with ChangeNotifier {
+  int selectedIndex = 0;
+  final List<Widget> pages = const [MealPage(), IngredientPage(), HealthPage()];
+  late Widget currentPage = pages[selectedIndex];
+  void selectTab(int index) {
+    selectedIndex = index;
+    currentPage = pages[index];
+    notifyListeners();
+  }
+}
 
 class MyApp extends StatelessWidget {
+  static const List<BottomNavigationBarItem> bottomBarItems = [
+    BottomNavigationBarItem(
+        icon: Icon(Icons.food_bank_rounded),
+        label: 'Meal',
+        tooltip: 'Log meals here'),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.shopping_basket_rounded),
+        label: 'Ingredient',
+        tooltip: 'Modify Ingredient Database'),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.medical_services_rounded),
+        label: 'Insulin',
+        tooltip: 'Enter insulin data here')
+  ];
   static const String applicationTitle = 'CarbOManager';
   static final ThemeData themeData = ThemeData(primarySwatch: Colors.blue);
 
--- carb_o_manager/lib/main_view.dart
+++ carb_o_manager/lib/main_view.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'main.dart';
+import 'package:provider/provider.dart';
 
 class MainView extends StatelessWidget {
   const MainView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello Flutter'),
+    return Scaffold(
+      appBar: AppBar(),
+      body: context.watch<MyAppModel>().currentPage,
+      bottomNavigationBar: BottomNavigationBar(
+        items: MyApp.bottomBarItems,
+        onTap: context.read<MyAppModel>().selectTab,
+        currentIndex: context.watch<MyAppModel>().selectedIndex,
       ),
     );
   }
